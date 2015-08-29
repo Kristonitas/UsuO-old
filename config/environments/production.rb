cab9b93544b0d57b514df670f49791a8ddfc7afc
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2015-08-29 22:46:51 +0300

    more email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,8 +65,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: 'usuo.lt', port: 80 }
   config.action_mailer.delivery_method = :sendmail
-  # Defaults to:
   config.action_mailer.sendmail_settings = {
     location: '/usr/sbin/sendmail',
     arguments: '-i',
