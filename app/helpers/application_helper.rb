659828698f2c543e43c35a9c59ff67aec97e999a
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2015-08-30 17:16:57 +0300

    paths for edit users added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def anyone_signed_in?
     student_signed_in? or orange_signed_in?
   end
+
+  def current_user
+    if anyone_signed_in?
+      student_signed_in? ? :student : :orange
+    end
+  end
 end
